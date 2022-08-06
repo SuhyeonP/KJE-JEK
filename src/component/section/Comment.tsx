6e313b8d12f112206d5cee1712cb82a68072f3ea
suhyeonP <ahah12k@gmail.com>
2022-08-06 10:02:56 +0900

    :art: add react hook form on comment

--- src/component/section/Comment.tsx
+++ src/component/section/Comment.tsx
@@ -1,16 +1,29 @@
 import styled from '@emotion/styled';
 import { MainTitle } from 'component/common';
 import { colorPalette } from 'color/colorPalette';
+import { useForm } from 'react-hook-form';
 
 const CommentStyled = styled.div``;
 
+interface IFormProps {
+  name: string;
+  comment: string;
+}
+
 export const Comment = (): JSX.Element => {
+  const { register, handleSubmit } = useForm<IFormProps>();
+
+  const comment = (data: IFormProps) => {
+    console.log(data);
+  };
+
   return (
     <CommentStyled>
       <MainTitle color={colorPalette.main_blue}>주언이와 은경이에게 전하는 말</MainTitle>
-      <form className="comment-form">
-        <input />
-        <input />
+      <form className="comment-form" onSubmit={handleSubmit(comment)}>
+        <input {...register('name')} />
+        <input {...register('comment')} />
+        <button>댓글 남기기</button>
       </form>
     </CommentStyled>
   );
