4f97f2df368b3bf86ec71b0d73b6c983e4a925c9
suhyeonP <ahah12k@gmail.com>
2022-08-06 09:48:32 +0900

    :construction: add foldable wedding info location

--- src/component/section/WeddingInfo.tsx
+++ src/component/section/WeddingInfo.tsx
@@ -1,6 +1,7 @@
 import styled from '@emotion/styled';
 import { MainTitle } from 'component/common';
 import { colorPalette } from 'color/colorPalette';
+import { useState } from 'react';
 
 const WeddingInfoStyled = styled.div`
   padding: 79px 0 0;
@@ -15,15 +16,6 @@ const WeddingInfoStyled = styled.div`
     }
     .special-no-thanks {
       font-size: 13px;
-    }
-
-    .go-map {
-      background-color: ${colorPalette.main_red};
-      color: white;
-      border: 0;
-      outline: 0;
-
-      padding: 21px 19px;
     }
   }
 
@@ -51,10 +43,24 @@ const WeddingInfoStyled = styled.div`
         }
       }
     }
+
+    .go-map {
+      display: flex;
+      justify-content: center;
+      & > button {
+        background-color: ${colorPalette.main_red};
+        color: white;
+        border: 0;
+        outline: 0;
+
+        padding: 21px 19px;
+      }
+    }
   }
 `;
 
 export const WeddingInfo = (): JSX.Element => {
+  const [isOpen, setIsOpen] = useState(false);
   const openMap = () => {
     console.log('go map');
   };
@@ -69,26 +75,33 @@ export const WeddingInfo = (): JSX.Element => {
         <p className="wedding-info-location">서울특별시 강남구 도산대로 38길 32 토브헤세드</p>
         <p className="wedding-info-dress">드레스코드: 단풍색 (레드, 그린, 머스타드, 브라운)</p>
         <p className="special-no-thanks">* 축하 화환은 정중히 사양합니다. 마음만 감사히 받겠습니다.</p>
-        <button onClick={openMap} className="go-map">
-          네이버지도 바로가기
-        </button>
       </div>
       <div className="map-img" />
-      <div role="ul" className="how-to-come">
-        <div role="li">
-          <p className="come-subway">지하철로 오실 때</p>
-          <p>7호선 학동역 10번 출구에서 셔틀버스 이용 혹은 도보 8분</p>
-        </div>
-        <div role="li">
-          <p className="come-bus">버스로 오실 때</p>
-          <p>- 145, 440, 4212번 버스 타고 언북중학교 입구 하차</p>
-          <p>- 141, 3600번 버스 타고 서울세관 하차</p>
-        </div>
-        <div role="li">
-          <p className="come-car">자가용으로 오실 때</p>
-          <p>식장 앞에서 주차 안내 예정</p>
-        </div>
+      <div className="toggle-wedding-info">
+        <button type="button" onClick={() => setIsOpen(prev => !prev)}>
+          찾아오시는 길 자세히 보기 {isOpen ? '^' : 'down'}
+        </button>
       </div>
+      {isOpen && (
+        <div role="ul" className="how-to-come">
+          <div role="li">
+            <p className="come-subway">지하철로 오실 때</p>
+            <p>7호선 학동역 10번 출구에서 셔틀버스 이용 혹은 도보 8분</p>
+          </div>
+          <div role="li">
+            <p className="come-bus">버스로 오실 때</p>
+            <p>- 145, 440, 4212번 버스 타고 언북중학교 입구 하차</p>
+            <p>- 141, 3600번 버스 타고 서울세관 하차</p>
+          </div>
+          <div role="li">
+            <p className="come-car">자가용으로 오실 때</p>
+            <p>식장 앞에서 주차 안내 예정</p>
+          </div>
+          <div className="go-map">
+            <button onClick={openMap}>네이버지도 바로가기</button>
+          </div>
+        </div>
+      )}
     </WeddingInfoStyled>
   );
 };
