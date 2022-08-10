697c83906772f05b877da700cefe0545ac5b6f43
suhyeonP <ahah12k@gmail.com>
2022-08-10 16:11:32 +0900

    :construction: before custom

--- src/component/section/WeddingPicture.tsx
+++ src/component/section/WeddingPicture.tsx
@@ -2,6 +2,26 @@ import styled from '@emotion/styled';
 import { MainTitle } from 'component/common';
 import { colorPalette } from 'color/colorPalette';
 
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import Slider from 'react-slick';
+
+const imgs = [
+  '4545-1',
+  '4561',
+  '4673',
+  '4812',
+  '4859',
+  '4867',
+  '4896',
+  '5028-1',
+  '5139',
+  '5265',
+  '5297',
+  '5331',
+  '5371',
+];
+
 const WeddingPictureStyled = styled.div`
   margin-bottom: 200px;
   .main-title {
@@ -11,18 +31,69 @@ const WeddingPictureStyled = styled.div`
 
   .wedding-picture {
     width: 100%;
-    height: 926px;
-    background-color: gray;
+    background-color: ${colorPalette.main_pink};
+  }
+
+  .wedding-img-slider {
+    & > img {
+      width: 100%;
+    }
+  }
+
+  .slick-list {
+    overflow-x: hidden;
+
+    .slick-track {
+      overflow-x: hidden;
+    }
+  }
+
+  .slick-slide {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+  }
+
+  .slick-dots {
+    width: 90%;
+    & > li {
+      width: 15px;
+      height: 15px;
+
+      & > button {
+        width: 15px;
+        height: 15px;
+
+        padding: 0;
+      }
+      :before {
+        width: 15px;
+        height: 15px;
+      }
+    }
   }
 `;
 
 export const WeddingPicture = (): JSX.Element => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    arrows: false,
+  };
+
   return (
     <WeddingPictureStyled>
       <div className="main-title">
         <MainTitle color={colorPalette.main_red}>웨딩 사진 보기</MainTitle>
       </div>
-      <div className="wedding-picture">wedding picture</div>
+      <div className="wedding-picture">
+        {imgs.map(ele => (
+          <div className="wedding-img-slider" key={ele}>
+            <img src={`assets/images/wedding/4V4A${ele}.jpg`} />
+          </div>
+        ))}
+      </div>
     </WeddingPictureStyled>
   );
 };
