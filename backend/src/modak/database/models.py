69c0ddd5c4a627b517b127f1fcba16512737fb03
Jooeon Kim <joo2theeon@gmail.com>
2022-08-16 01:47:12 +0900

    fix

--- backend/src/modak/database/models.py
+++ backend/src/modak/database/models.py
@@ -1,22 +1,3 @@
-#
-#  MAKINAROCKS CONFIDENTIAL
-#  ________________________
-#
-#  [2017] - [2022] MakinaRocks Co., Ltd.
-#  All Rights Reserved.
-#
-#  NOTICE:  All information contained herein is, and remains
-#  the property of MakinaRocks Co., Ltd. and its suppliers, if any.
-#  The intellectual and technical concepts contained herein are
-#  proprietary to MakinaRocks Co., Ltd. and its suppliers and may be
-#  covered by U.S. and Foreign Patents, patents in process, and
-#  are protected by trade secret or copyright law. Dissemination
-#  of this information or reproduction of this material is
-#  strictly forbidden unless prior written permission is obtained
-#  from MakinaRocks Co., Ltd.
-
-# pylint: disable=unused-wildcard-import,wildcard-import
-
 # Import all the models, so that Base has them before being imported by Alembic
 # Refs): https://stackoverflow.com/a/29218728
 from modak.comments.model import *  # noqa: F401,F403
