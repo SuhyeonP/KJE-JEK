25c8483af790627c6d48fb372cff51ec4031b49b
suhyeonP <ahah12k@gmail.com>
2022-08-17 14:28:08 +0900

    :art: add concat comment

--- frontend/src/component/section/comment/Comments.tsx
+++ frontend/src/component/section/comment/Comments.tsx
@@ -4,6 +4,7 @@ import { colorPalette } from 'color/colorPalette';
 import { useState } from 'react';
 import { Button } from 'component/common/Button';
 import { useQuery } from '@tanstack/react-query';
+import { nanoid } from 'nanoid';
 
 const CommentsStyled = styled.div`
   position: relative;
@@ -24,34 +25,32 @@ interface IComment {
 }
 
 const getComment = (page: number) => {
-  console.log(page);
   return fetch(`http://localhost:8080/v1/comments?page=${page}&page_size=3`).then(res => res.json());
 };
 
 export const Comments = (): JSX.Element => {
   const [total] = useState(10);
   const [page, setPage] = useState(1);
+  const [comments, setComments] = useState<IComment[]>([]);
 
   const getMoreComment = () => {
     setPage(prev => prev + 1);
   };
 
-  const { data } = useQuery<any, any, Record<'comments', IComment[]>>(
-    ['get-comments', page],
-    async () => await getComment(page),
-    {
-      onSuccess: data => {
-        //
-      },
-    }
-  );
+  useQuery<any, any, Record<'comments', IComment[]>>(['get-comments', page], async () => await getComment(page), {
+    onSuccess: data => {
+      setComments(prev => {
+        return prev.concat(data.comments);
+      });
+    },
+  });
 
   return (
     <CommentsStyled>
       <p className="comment-count">댓글 {total}</p>
-      {data?.comments.map(comment => (
+      {comments.map(comment => (
         <Comment
-          key={comment.id}
+          key={nanoid()}
           emoji={comment.emoji}
           writer={comment.author}
           comment={comment.content}
