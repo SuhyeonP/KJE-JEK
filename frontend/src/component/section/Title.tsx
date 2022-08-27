55504dcd7cb6d3604f11ec0c8477cfcd52d8fec6
Jooeon Kim <joo2theeon@gmail.com>
2022-08-27 12:50:20 +0900

    fix project modak title css

--- frontend/src/component/section/Title.tsx
+++ frontend/src/component/section/Title.tsx
@@ -3,8 +3,8 @@ import { colorPalette } from 'color/colorPalette';
 
 const TitleStyled = styled.div`
   position: sticky;
-  top: calc(-50vh + 48.43px);
-  padding-top: calc(50vh - 48.43px);
+  top: calc(-50vh + 38px);
+  padding-top: calc(50vh - 38px);
 
   background-color: ${colorPalette.main_pink};
   z-index: 100;
@@ -12,7 +12,7 @@ const TitleStyled = styled.div`
   text-align: center;
 
   .main-logo {
-    height: 48.43px;
+    height: 38px;
     vertical-align: middle;
     margin: 3px 0;
   }
